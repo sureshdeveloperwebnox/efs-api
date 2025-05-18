ef9f035c9f2a6c02cd79e2e5e70984effbac5789
= <=>
2025-05-18 22:13:12 +0530

    app js file front end url added

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:9875', 'http://localhost:6945', ], //only allow http://localhost:8081 to make requests
+  origin: ['http://localhost:9875', 'http://localhost:6945' ], //only allow http://localhost:8081 to make requests
   credentials: true, // 👈 Important for cookie/session sharing
 
 };
