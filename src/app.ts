af60fd5308e018b7ca31e0d1fa76bd1fa405daba
siva <msivapriya0312@gmail.com>
2025-06-11 16:41:52 +0530

    Cors setup

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Accept']
+  // allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Accept']
 };
 
 // Apply CORS middleware
